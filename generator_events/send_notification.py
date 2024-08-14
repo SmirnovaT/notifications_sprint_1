34a5443dfcdacf7af4e97b325dd309fe95bbf384
SmirnovaT <tanya2.93@mail.ru>
2024-08-14 06:40:29 +0300

    Добавила дату отправки и request_id в генераторе

--- generator_events/send_notification.py
+++ generator_events/send_notification.py
@@ -9,6 +9,7 @@ from generator_events.events import (
     generate_new_series,
     generate_new_registration,
     generate_all_users_event,
+    fake,
 )
 from src.core.logger import notification_logger
 
@@ -27,7 +28,7 @@ def send_event(event: dict, access_token: str) -> None:
         settings.notification_api_url,
         json=event,
         timeout=15,
-        cookies={"access_token": access_token},
+        cookies={"access_token": access_token, "X-Request-ID": fake.uuid4()},
     )
     notification_logger.info(response.status_code)
     notification_logger.info(response.text)
