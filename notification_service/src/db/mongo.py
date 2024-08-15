427bbdcac5a338598277dd7ead546b70a17ee32f
GreyGreyman <greygreyman@gmail.com>
2024-08-15 12:36:24 +0300

    fix event worker docker networking

--- notification_service/src/db/mongo.py
+++ notification_service/src/db/mongo.py
@@ -5,8 +5,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 mongo: Optional[AsyncIOMotorClient] = None
 
 
-def init_mongo():
-    return AsyncIOMotorClient()
+def init_mongo(host: str, port: int):
+    return AsyncIOMotorClient(host=host, port=port)
 
 
 def get_mongo_db(db_name: str):
